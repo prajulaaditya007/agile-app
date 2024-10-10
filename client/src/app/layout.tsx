604a487f1c8fed73ade173638ef1c47694394ca3
Prajul <prajulaadityaa@gmail.com>
2024-10-11 04:10:30 +0530

    file renamed

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import React from "react";
+import DashboardWrapper from "@/app/dashboardWrapper";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <DashboardWrapper>{children}</DashboardWrapper>
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-between p-24">
+            hi
+        </main>
+    );
+}
